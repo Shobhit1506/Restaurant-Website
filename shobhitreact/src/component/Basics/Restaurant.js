91ef7ce5c53063c976b9db691be120a3a882f7c9
Shobhit1506 <shobhitd137@gmail.com>
2023-07-08 21:19:24 +0530

    React project

---
+++ shobhitreact/src/component/Basics/Restaurant.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import "./style.css";
+import Menu from "./menuAPi";
+import MenuCArd from './menuCArd';
+import Navbar from "./Navbar";
+
+const uniqueList = [
+  ...new Set(
+    Menu.map((curElem) => {
+      return curElem.category;
+    })
+  ),
+  "All",
+];
+
+console.log(uniqueList);
+
+const Resturant = () => {
+  const [menuData, setMenuData] = useState(Menu);
+  const [menuList, setMenuList] = useState(uniqueList);
+
+  const filterItem = (category) => {
+    if (category === "All") {
+      setMenuData(Menu);
+      return;
+    }
+
+    const updatedList = Menu.filter((curElem) => {
+      return curElem.category === category;
+    });
+
+    setMenuData(updatedList);
+  };
+
+  return (
+    <>
+      <Navbar filterItem={filterItem} menuList={menuList} />
+      <MenuCArd menuData={menuData} />
+    </>
+  );
+};
+
+export default Resturant;
---
+++ shobhitreact/src/component/Basics/menuAPi.js
@@ -0,0 +1,87 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/bhature.jpg",
+      name: "Chole Bhature ",
+      category: "breakfast",
+      price: "50₹",
+      description:
+        "Chole bhature is a food dish popular in the Northern areas of the Indian subcontinent. It is a combination of chana masala (spicy white chickpeas) and bhatura/puri, a deep-fried bread made from maida."
+    },
+  
+    {
+      id: 2,
+      image: "images/corn.jpg",
+      name: "Corn",
+      category: "evening",
+      price: "20₹",
+      description:
+      'It is called as bhutta in desi language.very good in taste usually we eat bhutaa or sweet corn in rainy season.the joy of eating this bhutta comes at the streets of delhi with lemon and masala on it.',
+    },
+    {
+      id: 3,
+      image: "images/Kachori.jpg",
+      name: "Kachori",
+      category: "breakfast",
+      price: "20₹",
+      description:
+      'Kachori is a sweet and spicy deep-fried snack, originating in India subcontinent, and common in places with Indian diaspora and other South Asian diaspora. Alternative names for the snack include kachauri, kachodi and katchuri',
+    },
+    {
+      id: 4,
+      image: "../images/chola.jpg",
+      name: "chola",
+      category: "lunch",
+      price: "50₹",
+      description:
+        'Chana masala (also chole masala, or chole) is a chickpea curry originating in the Indian subcontinent.It is a staple dish in North Indian cuisine',
+    },
+    {
+      id: 5,
+      image: "../images/pizza.jpg",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "Pizza is a dish of Italian origin consisting of a usually round, flat base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients , which is then baked at a high temperature, traditionally in a wood-fired oven.[1]",
+    },
+    {
+      id: 6,
+      image: "../images/vegthali.jpg",
+      name: "Veg-thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+         'Thali (meaning "plate") or Bhojanam (meaning "full meal") is a round platter used to serve food in South Asia, Southeast Asia and the Caribbean. Thali is also used to refer to an Indian-style meal made up of a selection of various dishes which are served on a platter. ',
+    },
+    {
+      id: 7,
+      image: "../images/sweet.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "Indian sweets and desserts, also called mithai, a significant element in Indian cuisine. Indians are known for their unique taste and experimental behavior when it comes to food. Many Indian desserts are fried foods made with sugar, milk or condensed milk. Ingredients and preferred types of dessert vary by region. In the eastern part of India",
+    },
+    {
+      id: 8,
+      image: "../images/rajmarice.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        'Red beans and rice is an emblematic dish of Louisiana Creole cuisine (not originally of Cajun cuisine) traditionally made on Mondays with Kidney beans,vegetables, spices and pork bones as left over from Sunday dinner, cooked together slowly in a pot and served over rice,',
+    },
+    {
+      id: 9,
+      image: "../images/samosa.jpg",
+      name: "samosa",
+      category: "evening",
+      price: "10₹",
+      description:
+        'A samosa  is a fried South Asian pastry with a savoury filling, including ingredients such as spiced potatoes, onions, peas, meat or fish. It may take different forms, including triangular, cone, or half-moon shapes, depending on the region.',
+    },
+  ];
+  
+  export default Menu;
+  
